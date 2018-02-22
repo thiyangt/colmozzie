b8f0d49ef18b61a64b75902f51b270f490ed3c4b
thiyangt <thiyanga.talagala@monash.edu>
2018-02-22 22:09:47 +1100

    fix the error in documentation

--- R/data.R
+++ R/data.R
@@ -1,4 +1,5 @@
 #' Weekly notified dengue cases in Sri Lanka
+#'
 #' @description Weekly notified dengue cases and climate variables in Colombo districts of Sri Lanka from 2008/week-52 to 2014/week-21
 #' @format A data frame with 279 rows and 12 variables
 #' \describe{
@@ -9,11 +10,12 @@
 #' \item{TMAX}{maximum temperature of the week (Celcius)}
 #' \item{Tm}{minimum temperature of the week (Celcius)}
 #' \item{SLP}{Sea level pressure}
-#' \item{H}{Relative humidity (%)}
-#' \item{PP}{Precipitation amount(mm)}
-#' \item{VV}{Mean visibility(km)}
-#' \item{V}{Mean wind speed(km//h)}
+#' \item{H}{Relative humidity (\%)}
+#' \item{PP}{Precipitation amount (mm)}
+#' \item{VV}{Mean visibility (km)}
+#' \item{V}{Mean wind speed (km/h)}
 #' \item{VM}{Maximum sustained wind speed (Km/h)}
+#' }
 #' @source weekly epidemiological reports published by the Epidemiological Unit, Ministry of Health, Sri Lanka
 #'
 #' @examples
